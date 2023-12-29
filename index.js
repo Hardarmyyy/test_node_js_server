6a8c8ef54a4ade90dce0e4f644efadd31abad353
hardarmyyy <adeyemoadams005@gmail.com>
2023-12-29 05:19:35 +0100

    TEST: Testing new changes in the docker hub

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ PORT=3990
 
 
 app.get("/",(req,res)=>{
-    res.send("Hello world, let's go Cloud with Jenkins automation");
+    res.send("Hello world, let's go Cloud with Jenkins CI/CD to build a docker image");
 })
 
 app.listen(process.env.PORT || PORT,()=>console.log(`Server is listening on ${process.env.PORT}`))
